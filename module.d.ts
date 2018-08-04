23764d8e13941f017f1990241eb93d063318a20c
tiansin <tiansin@tiansin.com>
2018-08-04 11:13:05 +0800

    add model exception

--- module.d.ts
+++ module.d.ts
@@ -45,6 +45,9 @@ declare namespace NE {
 }
 
 declare module '@adonisjs/generic-exceptions' {
+    interface LogicalException extends NE.LogicalException {}
+    interface HttpException extends NE.HttpException {}
+
     interface InvalidArgumentException extends NE.InvalidArgumentException {
         /**
          * Throw an exception when there is a missing parameter
@@ -162,10 +165,176 @@ declare module '@adonisjs/generic-exceptions' {
         invoke(message : string, status? : number, code? : string): RuntimeException;
     }
 
-    const LogicalException: NE.LogicalException
-    const HttpException: NE.HttpException;
+    const LogicalException: LogicalException
+    const HttpException: HttpException;
     const InvalidArgumentException: InvalidArgumentException;
     const RuntimeException: RuntimeException;
 
     export { LogicalException, HttpException, InvalidArgumentException, RuntimeException };
+}
+
+declare module "@adonisjs/lucid/src/Exceptions" {
+    import GE = require("@adonisjs/generic-exceptions");
+    
+    /**
+     * Class to throw runtime exceptions
+     * 
+     * @class RuntimeException
+     * @constructor
+     */
+    interface RuntimeException extends GE.RuntimeException{
+            
+        /**
+         * This exception is raised when user is trying to use an
+         * undefined database connection
+         *
+         * @method missingDatabaseConnection
+         *
+         * @param  {String}                  name
+         *
+         * @return {Object}
+         */
+        missingDatabaseConnection(name : string): RuntimeException;
+            
+        /**
+         * This exception is raised when user is trying to query
+         * relationships from an unsaved model instance
+         *
+         * @method unSavedModel
+         *
+         * @param  {String}     name
+         *
+         * @return {Object}
+         */
+        unSavedModel(name : string): RuntimeException;
+            
+        /**
+         * This exception is raised when an undefined relation is
+         * fetched or referenced within the code
+         *
+         * @method undefinedRelation
+         *
+         * @param  {String}          relation
+         * @param  {String}          name
+         *
+         * @return {Object}
+         */
+        undefinedRelation(relation : string, name : string): RuntimeException;
+            
+        /**
+         * This exception is raised when nested relationships are not
+         * supported. `withCount` method is an example of same
+         *
+         * @method cannotNestRelation
+         *
+         * @param  {String}           relation
+         * @param  {String}           parent
+         * @param  {String}           method
+         *
+         * @return {Object}
+         */
+        cannotNestRelation(relation : string, parent : string, method : string): RuntimeException;
+
+        /**
+         * This exception is raised when you are trying to eagerload
+         * relationship for multiple times
+         *
+         * @method overRidingRelation
+         *
+         * @param  {String}           relation
+         *
+         * @return {Object}
+         */
+        overRidingRelation(relation : string): RuntimeException;
+            
+        /**
+         * This exception is raised when migrations are locked but
+         * still someone is trying to migrate the database.
+         *
+         * @method migrationsAreLocked
+         *
+         * @param  {String}            lockTable
+         *
+         * @return {Object}
+         */
+        migrationsAreLocked(lockTable : string): RuntimeException;
+    }
+
+    /**
+     * Class to lucid model related exceptions
+     * 
+     * @class ModelException
+     * @constructor
+     */
+    interface ModelException extends GE.LogicalException{
+        deletedInstance(name : string): ModelException;
+    }
+
+    /**
+     * Exception thrown when a row is not found using
+     * findOrFail style methods.
+     * 
+     * @class ModelNotFoundException
+     * @constructor
+     */
+    interface ModelNotFoundException extends GE.LogicalException {
+        raise(name : string): ModelNotFoundException;
+    }
+
+    /**
+     * Class to throw exceptions related to model
+     * relations
+     * 
+     * @class ModelRelationException
+     * @constructor
+     */
+    interface ModelRelationException extends GE.LogicalException{  
+        /**
+         * This exception is raised when an unsupported method
+         * is called on a model relation. Naturally `xxx` is
+         * not a function will be thrown, but we want to
+         * be more explicit that `xxx` is not a method
+         * for `yyy` relation.
+         *
+         * @method unSupportedMethod
+         *
+         * @param  {String}          method
+         * @param  {String}          relation
+         *
+         * @return {Object}
+         */
+        unSupportedMethod(method : string, relation : string): ModelRelationException;
+            
+        /**
+         * This exception is raised when related model method is
+         * executed for which the model needs to be persisted
+         * but is not
+         *
+         * @method unsavedModelInstance
+         *
+         * @param  {String}             message
+         *
+         * @return {Object}
+         */
+        unsavedModelInstance(message : string): ModelRelationException;
+            
+        /**
+         * Exception thrown when trying to set flags on pivot
+         * model instance and when pivotModel is explicitly
+         * defined
+         *
+         * @method pivotModelIsDefined
+         *
+         * @param  {String}            method
+         *
+         * @return {Object}
+         */
+        pivotModelIsDefined(method : string): ModelRelationException;
+    }
+    const RuntimeException: RuntimeException;
+    const ModelException: ModelException;
+    const ModelNotFoundException: ModelNotFoundException;
+    const ModelRelationException: ModelRelationException;
+
+    export { RuntimeException, ModelException, ModelNotFoundException, ModelRelationException };
 }
